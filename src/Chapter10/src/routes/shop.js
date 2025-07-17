f314eef4782da72b18fedc0c31a0dacfe0537c88
Julian Cotto <julian.cotto@charter.com>
2025-07-17 09:03:21 -0500

    Chapter 9 Complete Chapter 10 Framework Added

--- src/Chapter10/src/routes/shop.js
+++ src/Chapter10/src/routes/shop.js
@@ -10,14 +10,7 @@ router.get('/', shopController.getIndex);
 
 router.get('/products', shopController.getProducts);
 
-// dynamic route using colon to indicate anything can come after the "/"
-router.get('/products/:productId', shopController.getProduct);
-
 router.get('/cart', shopController.getCart);
-
-router.post('/cart', shopController.postCart);
-
-router.post('/cart-delete-item', shopController.postCartDeleteProduct);
 
 router.get('/orders', shopController.getOrders);
 
