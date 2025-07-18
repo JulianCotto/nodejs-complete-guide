5f064992e7e819bd528a6e9b89384333974672bd
Julian Cotto <julian.cotto@charter.com>
2025-07-18 10:05:32 -0500

    Completed Chapter 11

--- src/Chapter11/src/app.js
+++ src/Chapter11/src/app.js
@@ -7,6 +7,10 @@ const errorController = require('./controllers/error');
 const sequelize = require('./util/database');
 const Product = require('./models/product');
 const User = require('./models/user');
+const Cart = require('./models/cart');
+const CartItem = require('./models/cart-item');
+const Order = require('./models/order');
+const OrderItem = require('./models/order-item');
 
 const app = express();
 
@@ -16,16 +20,16 @@ app.set('views', 'views');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-    User.findByPk(1)
-        .then(user => {
-            req.user = user;
-            next();
-        })
-        .catch(err => console.log(err));
+  User.findByPk(1)
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
 });
 
 app.use('/admin', adminRoutes);
@@ -33,34 +37,36 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-// define both directions for clarity
-Product.belongsTo(User,
-    {
-        constraints: true,
-        onDelete: 'CASCADE'
-    });
+Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
 User.hasMany(Product);
+User.hasOne(Cart, { onDelete: 'CASCADE' });
+Cart.belongsTo(User);
+Cart.belongsToMany(Product, { through: CartItem });
+Product.belongsToMany(Cart, { through: CartItem });
+Order.belongsTo(User, { onDelete: 'CASCADE' });
+User.hasMany(Order);
+Order.belongsToMany(Product, { through: OrderItem, onDelete: 'CASCADE' });
 
-sequelize.sync()
-    .then(_ => {
-        return User.findByPk(1);
-    })
-    .then(user => {
-        if (!user) {
-            return User.create({
-                name: "Julian",
-                email: "devjcotto@gmail.com"
-            })
-        }
-        return user;
-    })
-    .then(user => {
-        console.log(user)
-        app.listen(3000);
-    })
-    .catch(err => {
-        console.log(err)
-    });
-
-
-
+sequelize
+  // .sync({ force: true })
+  .sync()
+  .then(result => {
+    return User.findByPk(1);
+    // console.log(result);
+  })
+  .then(user => {
+    if (!user) {
+      return User.create({ name: 'Max', email: 'test@test.com' });
+    }
+    return user;
+  })
+  .then(user => {
+    // console.log(user);
+    return user.createCart();
+  })
+  .then(cart => {
+    app.listen(3000);
+  })
+  .catch(err => {
+    console.log(err);
+  });
